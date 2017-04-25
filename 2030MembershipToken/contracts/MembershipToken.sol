7614b9aab92629ba83efafd14c0ac97c2745b6e1
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-04-25 11:02:09 +0200

    Create base contract class and project.

---
+++ 2030MembershipToken/contracts/MembershipToken.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.4.10;
+
+import './TeamAllocation.sol';
+import './SafeMath.sol';
+
+contract MembershipToken {
+  using SafeMath for uint;
+
+  string public constant name = "Twenty Thirty Alpha Club Token";
+  string public constant symbol = "TTA";
+  uint8 public constant decimals = 18;
+
+  uint256 public constant totaNumberOfToken=4000000;
+  uint256 public constant tokenCreationRate = 50;
+
+}
+
+
+
+/*
+Token Name: Twenty Thirty Alpha Club Token
+Abbreviation: TTA or TAC or ACT (is there a hard rule on 3 letters for ERC20?)
+Total number of tokens issued: 4,000,000 tokens
+No. of decimal places per token: 18 I think is normal?
+Nominal price per Token: 1 USD (NOTE: We could price it in ether if we want)
+Tokens reserved for Team: 600,000
+Period Team token is marked locked: 9 months, compulsory OR mark not-able to use for CryptX.
+
+// Continue from here down
+Total Tokens on offer: 3,400,000
+
+Total Token sale period: 16 days
+Minimum Token to sell within offer period: 1,000,000
+Team tokens are held in treasury marked as team and can be transferred to team members who can a) not move them for 9 months or b) sell them freely
+*/
---
+++ 2030MembershipToken/contracts/TeamAllocation.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.10;
+
+import './MembershipToken.sol';
+import './SafeMath.sol';
+
+contract TeamAllocation {
+  using SafeMath for uint;
+  uint256 constant totalAllocations = 600000;
+  MembershipToken tta;
+  uint256 unlockedAt;
+  mapping (address=>uint256) allocations;
+
+  function TeamAllocation() {
+    tta = MembershipToken(msg.sender);
+    // Locked time of approximately 9 months before team members are able to redeeem tokens.
+    unlockedAt = now + 9 * 30 days;
+
+    // This is an example for allocating to team members. Need to replace with actual addresses and check percentage for each team member.
+    allocations[0x00] =  120000;
+    allocations[0x00] =  120000;
+    allocations[0x00] =  120000;
+    allocations[0x00] =  120000;
+    allocations[0x00] =  120000;
+  }
+}
