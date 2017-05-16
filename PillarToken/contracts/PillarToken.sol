f34e90ff8bcaa7dd8f41edbea231d37f783a0832
Parthasarathy Ramanujam <zincoshine>
2017-05-16 11:00:58 +0530

    Added transferFrom and approve functions

--- PillarToken/contracts/PillarToken.sol
+++ PillarToken/contracts/PillarToken.sol
@@ -54,6 +54,9 @@ contract PillarToken is ERC20Interface {
 
     mapping (address => uint256) balances;
 
+    //Owner of account approves the transfer of an amount to another account
+    mapping(address => mapping (address => uint256)) allowed;
+
     //event Approval(address indexed _owner, address indexed _spender,uint _value);
     //event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Refund(address indexed _from,uint256 _value);
@@ -180,6 +183,29 @@ contract PillarToken is ERC20Interface {
             return true;
         }
         return false;
+    }
+
+    function transferFrom(address _from, address _to, uint _amount) returns (bool success) {
+      if(balances[_from] >= _amount
+        && allowed[_from][msg.sender] >= _amount
+        && _amount > 0
+        && balances[_to] + _amount > balances[_to]) {
+          balances[_from] -= _amount;
+          allowed[_from][msg.sender] -= _amount;
+          balances[_to] += _amount;
+          Transfer(_from, _to, _amount);
+          return true;
+        } else {
+          return false;
+        }
+    }
+
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+    // If this function is called again it overwrites the current allowance with _value.
+    function approve(address _spender, uint256 _amount) returns (bool success) {
+      allowed[msg.sender][_spender] = _amount;
+      Approval(msg.sender, _spender, _amount);
+      return true;
     }
 
     // token migration
